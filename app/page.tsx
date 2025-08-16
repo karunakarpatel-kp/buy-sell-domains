169bbff88215d3c674ba1cfd30bdc11d14d5b479
Karunakar_Patel <karunakarpatel.kp@gmail.com>
2025-08-16 10:57:10 +0530

    Updated some code for reusuablity

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { CustomMetaData } from "../components/MetaData/CustomMetaData";
-import ListingCard from "@Components/ListingCards/ListingCard";
 import RainfallAnimation from "@Components/Animations/RainfallAnimation";
 
 export const metadata: any = CustomMetaData({ presentURL: SEO_OBJ.HOME_PAGE.absoluteURL });
@@ -13,9 +12,7 @@ const Home = () => {
     <>
       {/* Confetti */}
       <RainfallAnimation />
-      <div className="border-0 border-black ">
-        <ListingCard />
-      </div>
+      <div className="border-0 border-black "></div>
     </>
   );
 };
